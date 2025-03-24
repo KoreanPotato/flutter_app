5a58c73d2d17f72de5c1047976943f5e12e39e45
SergeyLim <limsergeystudy@gmail.com>
2025-03-24 14:16:37 +0100

    v 10

--- my_app/android/app/build.gradle.kts
+++ my_app/android/app/build.gradle.kts
@@ -3,10 +3,11 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
 android {
-    namespace = "com.example.my_app"
+    namespace = "hello.get.name"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
